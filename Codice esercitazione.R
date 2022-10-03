361be338ce5f3c46c3ad3b371db22e14ff63178e
Martin Filippone <m.filippone@studenti.unibg.it>
2022-10-03 11:08:39 +0200

    Librerie

---
+++ Codice esercitazione.R
@@ -0,0 +1,29 @@
+### Inizializzo Global Environment ###
+rm(list=ls())
+
+
+### Installazione dei pacchetti ###
+install.packages("devtools")
+
+install.packages('tinytex') #doc https://rmarkdown.rstudio.com/lesson-1.html#
+install.packages('rmarkdown')
+install.packages('devtools')
+
+
+
+
+### Inizializzazione pacchetti ###
+library("devtools")
+library('tinytex')
+library('rmarkdown')
+library('devtools')
+
+
+
+#setto la directory (Martin)
+setwd("C://Users/mfilippone/Documents/PW-Economia-reti-e-servizi")
+
+
+
+
+
